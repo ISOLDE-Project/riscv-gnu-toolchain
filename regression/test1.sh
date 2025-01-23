e261d2258a2bee24a4d41371cd3d84071e8b3421
darotsr <daro@tsr>
2025-01-23 22:26:08 +0200

    WIP: llvm/test/CodeGen/Conti/02_bug.ll

---
+++ regression/test1.sh
@@ -0,0 +1,2 @@
+llc  -mtriple=riscv32 -mattr=+v -target-abi=ilp32d -filetype=asm -o - /home/uic52463/hdd1/tristan-project/riscv-gnu-toolchain/llvm/llvm/test/CodeGen/ISOLDE/test_q.ll
+#llc  -mtriple=riscv32 -mattr=+v -target-abi=ilp32d -filetype=asm -o - /home/uic52463/hdd1/tristan-project/riscv-gnu-toolchain/llvm/llvm/test/CodeGen/ISOLDE/test_q1.ll
