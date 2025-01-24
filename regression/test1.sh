f1dbf8a33c5d0c995565296ac63ad61f889558a2
darotsr <daro@tsr>
2025-01-24 14:42:03 +0200

    QWORD test ok

--- regression/test1.sh
+++ regression/test1.sh
@@ -1,2 +1,6 @@
-llc  -mtriple=riscv32 -mattr=+v -target-abi=ilp32d -filetype=asm -o - /home/uic52463/hdd1/tristan-project/riscv-gnu-toolchain/llvm/llvm/test/CodeGen/ISOLDE/test_q.ll
-#llc  -mtriple=riscv32 -mattr=+v -target-abi=ilp32d -filetype=asm -o - /home/uic52463/hdd1/tristan-project/riscv-gnu-toolchain/llvm/llvm/test/CodeGen/ISOLDE/test_q1.ll
+#llc  -mtriple=riscv32 -mattr=+v -target-abi=ilp32d -filetype=asm -o - $ROOT_DIR/llvm/llvm/test/CodeGen/ISOLDE/test_q.ll
+#llc  -mtriple=riscv32 -mattr=+v -target-abi=ilp32d -filetype=asm -o - $ROOT_DIR/llvm/llvm/test/CodeGen/ISOLDE/test_q1.ll
+
+
+llvm-lit  $ROOT_DIR/llvm/llvm/test/CodeGen/ISOLDE/test_q.ll $ROOT_DIR/llvm/llvm/test/CodeGen/ISOLDE/test_q1.ll
+#llvm-lit   $ROOT_DIR/llvm/llvm/test/CodeGen/ISOLDE/test_q1.ll
